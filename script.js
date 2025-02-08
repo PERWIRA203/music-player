870913d15e72f99b3a1b9059724f31fd4e517450
PERWIRA203 <140954608+PERWIRA203@users.noreply.github.com>
2025-02-08 23:37:28 +0700

    music player

---
+++ script.js
@@ -0,0 +1,49 @@
+const content = [{
+    img: "picture/bad.jpg",
+    title: "Lady Gaga - Bad Romanced",
+    music: "music/Lady Gaga - Bad Romance (Lyrics).mp3",
+},
+{
+    img: "picture/locked.jpg",
+    title: "Bruno Mars - Locked Out Of Heaven",
+    music: "music/Bruno Mars - Locked Out Of Heaven.mp3",
+},
+{
+    img: "picture/wolves.jpg",
+    title: "Selena Gomez Marshmello - Wolves",
+    music: "music/Selena Gomez Marshmello - Wolves (Lyrics).mp3",
+},
+{
+    img: "picture/lovers_do.jpg",
+    title: "Maroon 5 - What Lovers Do feat. SZA",
+    music: "music/Maroon 5 - What Lovers Do (Lyrics) feat. SZA.mp3",
+},
+{
+    img: "picture/lavida.jpg",
+    title: "Coldplay - Viva la Vida",
+    music: "music/Coldplay - Viva la Vida (Lyrics).mp3",
+},
+{
+    img: "picture/brothers.jpg",
+    title: "Avicii - Hey Brother",
+    music: "music/Avicii - Hey Brother (Lyrics).mp3",
+},
+];
+const main = document.getElementById("main");
+
+main.innerHTML = content.map(music => 
+    `
+    <div class="container">
+            <div class="image">
+                <img src="${music.img}" alt="">
+            </div>
+            <div class="title">
+                <h5>${music.title}</h5>
+            </div>
+            <div class="loader">
+                <audio controls class="audio-player">
+                    <source src="${music.music}" type="audio/mp3">
+                  </audio>
+            </div>
+    </div>
+    `).join("");
